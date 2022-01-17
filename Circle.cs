6eea68c67ea177fe2b0de30cc12740292ea17824
tess <92467740+tessssssssss@users.noreply.github.com>
2022-01-17 17:32:00 +0100

    Circle

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_CSharpGame
+{
+    internal class Circle //reference when called
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Circle()
+        {
+            //constructor
+            x = 0;
+            y = 0;
+        }
+    }
+}
